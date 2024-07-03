fb861898683b05fbe0314ce99f895c57813fab5e
Realharpi <harpiwarrach@gmail.com>
2024-07-03 23:11:33 +0200

    Making it responsive for all laptops, tablets and mobiles phones except below 400px.

--- public/responsive.js
+++ public/responsive.js
@@ -1,20 +1,30 @@
 const responsive = () => {
     getViewWidth = window.innerWidth
     getViewHeight = window.innerHeight
-    console.log(getViewWidth)
     getViewDiff = (getViewWidth / 16)
     getResult = getViewHeight / getViewDiff
-    console.log(getResult)
+    // console.log(getViewWidth)            This is for testing purpose only.
+    console.log(getResult)               //This is for testing purpose only.
     // 8 is my monitor. 7.7 is my laptop. 7.2 is with bookmark. 
-    burgerSection = document.getElementById("burgers_section")
+    burgersSection = document.getElementById("burgers_section")
+    drinksSection = document.getElementById("drinks_section")
+    countdownSection = document.getElementById("countdown_section")
+    googleMapsSection = document.querySelector(".googleMaps_section")
 
-    console.log(burgerSection)
-    if(getResult < 7 ) {
-        burgerSection.style.height = "calc(100vh + 15rem)"
-        console.log("Done!")
-    } else if (getResult < 6)  {
-        burgerSection.style.height = "calc(100vh + 18rem)"
+    if(getResult < 7.5 && getResult > 6.6) {
+        burgersSection.style.height = "calc(100vh + 15rem)"
+    } else if (getResult < 6.5 && getResult > 5.6)  {
+        burgersSection.style.height = "calc(100vh + 20rem)"
+        drinksSection.style.height = "calc(100vh + 15rem)"
+        googleMapsSection.style.height = "calc(100vh + 15rem)"
+    } else if (getResult < 5.5 )  {
+        burgersSection.style.height = "calc(100vh + 30rem)"
+        drinksSection.style.height = "calc(100vh + 20rem)"
+        countdownSection.style.height = "calc(100vh + 15rem)"
+        googleMapsSection.style.height = "calc(100vh + 20rem)"
     }
 }
 
 responsive();
+
+window.addEventListener("resize", responsive)
